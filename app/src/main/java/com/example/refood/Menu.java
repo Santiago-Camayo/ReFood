a8a5b3377efe3c89c4028a23fc7ba9fe698b6068
Ozzaru-28 <alejandro.hurtado.m@uniautonoma.edu.co>
2025-04-01 20:36:26 -0500

    hecho por alejandro. Muchachos me quiero matar hpta proyecto tan duro, no sé como hacerlo más bonitoo :(

--- app/src/main/java/com/example/refood/Menu.java
+++ app/src/main/java/com/example/refood/Menu.java
@@ -1,25 +1,29 @@
 package com.example.refood;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
 
 public class Menu extends AppCompatActivity {
+
+    private ImageButton btnConfiguraciones;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_menu);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        btnConfiguraciones = findViewById(R.id.btnConfiguraciones);
+
+        btnConfiguraciones.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Menu.this, Configuraciones.class);
+                startActivity(intent);
+            }
         });
     }
 }
